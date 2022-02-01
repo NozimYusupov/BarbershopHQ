3f0e7e01bcd2536bc9cb29f5be799683b78cb735
Nozim Yusupov <nozimy@yyandex.ru>
2022-02-01 21:01:06 +0500

    Add validation

--- app.rb
+++ app.rb
@@ -7,7 +7,11 @@ require 'sinatra/activerecord'
 #set :database, "sqlite3:barbershop.db"
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 class Client < ActiveRecord::Base
-    
+  validates :name, presence: true    
+  validates :phone, presence: true
+  validates :datestamp, presence: true    
+  validates :color, presence: true
+
 end 
 
 class Barber < ActiveRecord::Base
@@ -28,20 +32,9 @@ get '/visit' do
 end
 
 post '/visit' do 
-  @username = params[:username]
-  @phone = params[:phone]
-  @datetime = params[:datetime]
-  @barber = params[:barber] 
-  @color = params[:color]
-
-  c = Client.new 
-  c.name = @username
-  c.phone = @phone
-  c.datestamp = @datetime
-  c.barber = @barber
-  c.color = @color
+  c = Client.new params[:client] 
   c.save
-
+  
   erb "<h2>Ok. Your are recorded</h2>"
 end
 
